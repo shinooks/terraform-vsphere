b09908c47f9cddab3d175635f16d15b31f992a2f
hsw <tls8204@gmail.com>
2025-04-03 14:37:53 +0900

    output 파일 분리

--- 1. vSphere 필수 데이터 추출/data.tf
+++ 1. vSphere 필수 데이터 추출/data.tf
@@ -23,35 +23,3 @@ data "vsphere_network" "nw" {
 data "vsphere_host" "host" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-
-
-output "datacenter_id" {
-  value = data.vsphere_datacenter.dc.id
-}
-
-output "host_id" {
-  value = data.vsphere_host.host.id
-}
-output "host_resource_pool" {
-  value = data.vsphere_host.host.resource_pool_id
-}
-
-output "resource_pool_id" {
-  value = data.vsphere_resource_pool.pool.id
-}
-
-output "datastore_id" {
-  value = data.vsphere_datastore.ds.id
-}
-
-output "datastore_stats" {
-  value = data.vsphere_datastore.ds.stats
-}
-
-output "network_id" {
-  value = data.vsphere_network.nw.id
-}
-
-output "network_type" {
-  value = data.vsphere_network.nw.type
-}
